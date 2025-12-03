a5fd2a1301d22fa97b734921f13f8a6ba7a81356
victorybh04 <victorybh04@naver.com>
2025-12-03 16:59:28 +0900

    feat: 환경 변수 설정을 위한 .env.example 파일 추가 및 서버 포트 설정 개선

--- Findit Backend/server-api-test.js
+++ Findit Backend/server-api-test.js
@@ -3,12 +3,13 @@ const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
+require('dotenv').config();
 
-const port = 3000;
+const port = process.env.PORT;
 const FAKE_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36';
 
-const NAVER_CLIENT_ID = 'RvwXB1LO1GIEynFybmUO';
-const NAVER_CLIENT_SECRET = '6bnhmXlL8s';
+const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
+const NAVER_CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
 
 app.use(cors());
 
@@ -27,29 +28,36 @@ app.get('/search', async (req, res) => { // '/search' api endpoint
     try {
         const gmarketUrl = `https://browse.gmarket.co.kr/search?keyword=${encodedSearchQuery}`;
         const coupangUrl = `https://www.coupang.com/np/search?&q=${encodedSearchQuery}`;
-        const naverApiUrl = `https://openapi.naver.com/v1/search/shop.json?query=${encodedSearchQuery}&display=1&exclude=used:rental`;   // 일단 최상단 1개만 가져옴, 중고와 렌탈상품 제외
+        const naverApiUrl = `https://openapi.naver.com/v1/search/shop.json`;
         // TODO: 검색 쿼리 최적화 (쓸모없는 부분 삭제, 브랜드 및 모델명 강조 검색)
 
         const naverResponse = await axios.get(naverApiUrl, {
+            params: {
+                query: searchQuery,     // params 문법에서는 자동으로 query 인코딩
+                display: 1,             // 일단 최상단 1개 상품만 가져옴
+                exclude: 'used:rental'  // 중고 및 렌탈상품 제외
+            },
             headers: {
                 'X-Naver-Client-Id': NAVER_CLIENT_ID,
                 'X-Naver-Client-Secret': NAVER_CLIENT_SECRET
             },
             timeout: 5000
         });
-        const naverResData = naverResponse.data.items[0];
-        
         const productResult = [];
         
-        productResult.push({
-            storeName: ['네이버스토어', 'naverstore'],
-            title: naverResData.title,
-            price: parseInt(naverResData.lprice),
-            url: naverResData.link,
-            shippingFeeText: '🛒 링크에서 직접 확인'   // 네이버 API는 배송비 정보 X
-        });
+        if (naverResponse.data.items && naverResponse.data.items.length > 0){
+            const naverResData = naverResponse.data.items[0];
+            
+            productResult.push({
+                storeName: ['네이버스토어', 'naverstore'],
+                title: naverResData.title.replace(/<[^>]*>?/g, ''),
+                price: parseInt(naverResData.lprice),
+                url: naverResData.link,
+                shippingFeeText: '🛒 링크에서 직접 확인'   // 네이버 API는 배송비 정보 X
+            });
+        }
         
-        console.log(naverResData);
+        console.log(productResult);
         // TODO: 최적의 일치상품 하나만 찾아내는 로직 필요
         res.json({ results: productResult });
     }
